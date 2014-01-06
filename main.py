fff3c02bca5c30ca94210dbf9d4f1d9ea73b199b
scott1028 <mic1028002@gmail.com>
2014-01-06 20:23:55 +0800

    update readme

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 # coding:utf-8
-# 所有被引入的子目錄 import 的根據都是跟這這份文件的位置。
-# 所以可以在 m3/m3.py 內用 import m1.m1 或 from m1.m1 import *
 
-# 很常見的引入
-# from m1.m1 import *
-# from m2.m2 import *
+import unittest
 
-# 巢狀的引入, 內部可再根據 App 執行檔的根目錄路徑做引入
-# from ... import 的是 class 
-from m3.m3 import *
+class test_import(unittest.TestCase):
+	def setUp(self):
+		print '\n[*] prepare for per test case...\n'
 
-a=m2()
+	def try_test_import(self):
+		# 所有被引入的子目錄 import 的根據都是跟這這份文件的位置。
+		# 所以可以在 m3/m3.py 內用 import m1.m1 或 from m1.m1 import *
 
-print a
+		# 很常見的引入
+		# from m1.m1 import *
+		# from m2.m2 import *
 
-# import module
-# from {module_path} import {class} # 而且可以正常運作(包含那個文件內的所有靜態資料或方法)
-import m3.m3
+		# 巢狀的引入, 內部可再根據 App 執行檔的根目錄路徑做引入
+		# from ... import 的是 class 
+		from m3.m3 import *
 
-print dir(m3.m3)
-print m3.m3.m3_function
-print m3.m3.m3
+		a=m2()
 
-from m3.m3 import m3_function
+		print a
 
-print m3_function
+		# import module
+		# from {module_path} import {class} # 而且可以正常運作(包含那個文件內的所有靜態資料或方法)
+		import m3.m3
+
+		print dir(m3.m3)
+		print m3.m3.m3_function
+		print m3.m3.m3
+
+		from m3.m3 import m3_function
+
+		print m3_function
+
+if __name__ == '__main__':
+    unittest.main()
